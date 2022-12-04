5d4fa082f8ab10f6067e106ce907befe3a98b3aa
Miika Mäkiniemi <miika@refined.com>
2022-12-04 16:44:08 +0200

    Day3

---
+++ 2022/Day3.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const buffer = fs.readFileSync(path.resolve(__dirname, './inputs/Day3.txt'));
+const inputs = buffer.toString().split('\n');
+
+const getItemScore = (item) => {
+  const charCode = item.charCodeAt();
+  if (item === item.toUpperCase()) {
+    return (charCode - 38)
+  }
+  return (charCode - 96)
+}
+
+let result = 0;
+
+for (const input of inputs) {
+  const inputArray = input.split('');
+  const half = inputArray.length / 2;
+  const rucksack1 = inputArray.slice(0, half);
+  const rucksack2 = inputArray.slice(half);
+  const duplicateItem = rucksack1.find(item => rucksack2.includes(item));
+  result += getItemScore(duplicateItem);
+}
+
+console.log('Answer Day 3, Part 1: ', result);
+
+result = 0;
+
+for (let i = 0; i < inputs.length; i += 3) {
+  const elf1 = inputs[i].split('');
+  const elf2 = inputs[i + 1].split('');
+  const elf3 = inputs[i + 2].split('');
+  const duplicateItem = elf1.find(item => elf2.includes(item) && elf3.includes(item));
+  result += getItemScore(duplicateItem);
+}
+
+console.log('Answer Day 3, Part 2: ', result);
