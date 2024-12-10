ed72927a320057b5f289b1998a90ea15f8ba8c0f
Miika Mäkiniemi <miika@refined.com>
2024-12-10 13:58:35 +0200

    wip

---
+++ 2023/Day2.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+const buffer = fs.readFileSync('./2023/inputs/Day2.txt');
+const inputs = buffer.toString().split('\n');
+
+let sum = 0;
+
+
+console.log('Answer Day 2, Part 1: ', sum);
+
+
+console.log('Answer Day 2, Part 2: ', sum);
